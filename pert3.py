5bb6c776c3aced1996e1546bf39262e8db6f39bc
laleandini <lalekartikaayuandini86@gmail.com>
2025-11-15 10:54:47 +0700

    Add files via upload

---
+++ pert3.py
@@ -0,0 +1,13 @@
+f = 6 # bilangan bulat ( integer "int")
+print("nilai dari f :", f)
+print(type(f))
+
+i = 2.2 # bilangan pecahan (float "float")
+print("nilai dari i :", i)
+print(type(i))
+
+n = februari # tipe data string (tulisan "str")
+print("nilai dari n :", n)
+print(type(n))
+
+a = # tipe data benar salah (true/false "bool")
---
+++ pert4.py
@@ -0,0 +1,22 @@
+# mengambil inputan dari user
+
+# = input("masukkan nama anda : ")
+#print("selamat datang ", f)
+#print(type(f))
+
+#i = int(input("masukkan umur anda : "))
+#print("umur anda sekarang", i)
+#print(type(i))
+
+#n = bool(input("apakah kamu selingkuh? : "))
+#print("kejujuranmu", n)
+#print(type(n))
+
+# TUGAS
+
+nama = input("masukkan nama anda : ")
+nim = int(input("masukkan nim saya : "))
+print("nama saya", nama)
+print("nim saya ", nim)
+print(type(nama))
+print(type(nim))
