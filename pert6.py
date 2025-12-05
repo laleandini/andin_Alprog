81dad15625f82a759e349fab6d765211218e3d86
laleandini <lalekartikaayuandini86@gmail.com>
2025-12-05 10:07:51 +0700

    pert6.py
    This program converts temperatures from Celsius to Fahrenheit, Reamur, and Kelvin based on user input.

---
+++ pert6.py
@@ -0,0 +1,32 @@
+# program konversi suhu
+
+celcius = float(input(("masukan suhu anda :")))
+print("suhu anda dalam celcius :",celcius )
+
+# fahrenheit x 9/5 celcius + 32
+fahrenheit = (9/5 * celcius) + 32
+print("suhu anda dalam fahrenheit :"fahrenheit ) 
+# reamur
+# kelvin 
+
+
+fahrenhit = float(input(("masukan suhu anda :")))
+print("suhu anda dalam fahrenhit :",fahrenhit )
+      
+ # celcius
+ # reamur
+ # kelvin
+
+ reamur = float(input(("masukan suhu anda :")))
+print("suhu anda dalam reamur :",reamur )
+
+# celcius
+# fahrenhit
+# kelvin
+
+kelvin = float(input(("masukan suhu anda :")))
+print("suhu anda dalam kelvin :",kelvin )
+
+# celcius
+# reamur
+# kelvin
