86d12e15e4c0008d71d49153cd4d64ac99008c0d
laleandini <lalekartikaayuandini86@gmail.com>
2025-12-05 10:06:59 +0700

    pert5.py
    Implement various arithmetic operations including addition, subtraction, multiplication, division, exponentiation, modulus, and floor division.

---
+++ pert5.py
@@ -0,0 +1,51 @@
+# operasi aritmatika
+# x / + -
+
+# operasi penjumlahan +
+a = 9
+b = 12
+hasil = a + b
+print(a, "+", b, "=", hasil)
+
+# pengurangan -
+
+# perkalian x
+
+# pembagian /
+
+# perpangkatan ** 
+
+# perpangkatan **
+hasil = a * b
+print(a, "x", b, "=", hasil)
+
+# pengurangan -
+hasil = a - b
+print(a, "-", b, "=", hasil)
+
+# pembagian /
+hasil = a / b
+print(a, ":", b, "=", hasil)
+
+# perpangkatan ** (exsponesial)
+hasil = a ** b
+print(a, "**", b, "=", hasil)
+
+# sisa pembagian (modulus) %
+a = 12
+b = 9
+hasil = a % b
+print(a,"mod", b, "=", hasil)
+
+# floor division
+a = 15
+b = 4
+hasil =a // b
+print(a, "floor division", b,"=",hasil)
+
+# operasi prioritas
+# 1 ()
+# perpangkatan pembagian
+# perkalian pembagian
+# penjumlahan pengurangan
+# buatlah operasi penjumlah yang menggabungkan semua operasi diatas
